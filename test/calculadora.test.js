87ac9f23305695cd7ac3822bb686c40138ffcab3
FelipeMessaMarques <felipembmarques@gmail.com>
2025-10-14 17:52:57 -0300

    arquivos das aulas de teste e protocolos HTTP

--- test/calculadora.test.js
+++ test/calculadora.test.js
@@ -1,3 +1,17 @@
-test("Espero que 1 seja 1", () => {
-  expect(1).toBe(1)
+const calculadora = require("../models/calculadora.js");
+
+
+test("A soma de 2 + 2 = 4", () => {
+  const resultado = calculadora.somar(2, 2);
+  expect(resultado).toBe(4);
+}); 
+
+test("A soma de 5 + 100 = 105", () => {
+  const resultado = calculadora.somar(5, 100);
+  expect(resultado).toBe(105);
+});
+
+test("'melancia' + 100 = 'ERRO'", () => {
+  const resultado = calculadora.somar("melancia", 100);
+  expect(resultado).toBe("ERRO")
 });
